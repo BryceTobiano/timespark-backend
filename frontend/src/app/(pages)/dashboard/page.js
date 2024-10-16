4687393b8e17122c50d4fb3193f0dc1c511bee23
Bryce Tobiano <tobiano@usc.edu>
2024-10-15 21:42:01 -0700

    Redirect home directory

--- frontend/src/app/(pages)/dashboard/page.js
+++ frontend/src/app/(pages)/dashboard/page.js
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import styles from "./dashboard.module.css";
 
 export default function Home() {
   return (
     <div>
       <main className={styles.main}>
-        <h1>Login page</h1>
+        <h1>Dashboard Page</h1>
       </main>
 
     </div>
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,13 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import { redirect } from 'next/navigation'
 
 export default function Home() {
+  redirect(`/login`) 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Home page</h1>
+        
       </main>
     </div>
   );
