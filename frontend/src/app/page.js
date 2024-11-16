1c1dcd9e7e8e3ea907a5af49ffb8bff4c34e120e
Bryce Tobiano <tobiano@usc.edu>
2024-11-15 18:12:03 -0800

    Redirects to coming soon page, add favicon

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,7 +2,7 @@ import styles from "./page.module.css";
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-  redirect(`/login`) 
+  redirect(`/coming_soon`) 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
