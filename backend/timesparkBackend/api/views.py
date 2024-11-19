59e53540d6e825615fef32408a39bd8cb14139fa
Bryce Tobiano <tobiano@usc.edu>
2024-11-18 16:42:54 -0800

    Able to Register/Login generating JWT Tokens

--- backend/timesparkBackend/api/views.py
+++ backend/timesparkBackend/api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import User
+from .serializers import UserSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+# View users, for debugging 
+class ListUsersView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import MyTokenObtainPairSerializer
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    queryset = User.objects.all()
+    serializer_class = MyTokenObtainPairSerializer
