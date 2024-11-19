783312d6b2532636034a3cef771ebe30c1f680c8
Bryce Tobiano <tobiano@usc.edu>
2024-11-18 16:42:54 -0800

    Able to Register/Login generating JWT Tokens

--- backend/timesparkBackend/timesparkBackend/urls.py
+++ backend/timesparkBackend/timesparkBackend/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from api.views import MyTokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path("api/token/", MyTokenObtainPairView.as_view(), name="get_token"),
+    path("api/token/refres/", TokenRefreshView.as_view(), name="refresh"),
 ]
