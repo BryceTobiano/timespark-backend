8e517429ed1c4abe1dee83e378c6578d3ab5fb19
Bryce Tobiano <tobiano@usc.edu>
2024-11-21 15:12:20 -0800

    Backend attempt vercel deployment

--- backend/timesparkBackend/api/urls.py
+++ backend/timesparkBackend/api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import CreateUserView, ListUsersView, GoogleCalendarInitView, GoogleCalendarRedirectView
+from .views import index, CreateUserView, ListUsersView, GoogleCalendarInitView, GoogleCalendarRedirectView
 
 urlpatterns = [
+    path('/', CreateUserView, name="HelloWorld"),
     path('register/', CreateUserView.as_view(), name="register"),
     path('users/', ListUsersView.as_view(), name="users"),
     path('google/auth', GoogleCalendarInitView.as_view(), name="google"),
--- backend/timesparkBackend/api/views.py
+++ backend/timesparkBackend/api/views.py
@@ -47,6 +47,22 @@ load_dotenv()
 CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 
+
+from django.http import HttpResponse
+
+
+def index(request):
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
+
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
