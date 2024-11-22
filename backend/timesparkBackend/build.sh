469ba01c47706baa4479aa17e6d17c55d4f4eed0
Bryce Tobiano <tobiano@usc.edu>
2024-11-22 00:40:54 -0800

    update build.sh

--- backend/timesparkBackend/build.sh
+++ backend/timesparkBackend/build.sh
@@ -2,9 +2,9 @@
 
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip3 install -r requirements.txt
 
-echo "Make Migration" 
+echo "Make Migrations..." 
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
