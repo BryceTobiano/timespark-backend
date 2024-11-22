377d9cd4329bbf0904fbc7cf9ed6137eb685db5b
Bryce Tobiano <tobiano@usc.edu>
2024-11-22 00:46:54 -0800

    Changing python version

--- backend/timesparkBackend/build.sh
+++ backend/timesparkBackend/build.sh
@@ -3,13 +3,13 @@
 # Build the project
 echo "Building the project..."
 # create a virtual environment named 'venv' if it doesn't already exist
-python3.9 -m venv env
+python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
 
 echo "Make Migrations..." 
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "Collect Static..." 
-python3.9 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
